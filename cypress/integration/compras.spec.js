ac19e2754363fed49268b0639d5f0dd8d71cbb41
Quintiliano Paquini Nery <quintiliano@tecsystem.com.br>
2020-12-11 16:05:31 -0300

    background login

--- cypress/integration/compras.spec.js
+++ cypress/integration/compras.spec.js
@@ -2,9 +2,17 @@
 
 context('Compras', () => {
     it('Efetuar uma compra do produto e verificar seu código na lista de pedidos', () => {
+
+        //cookie de autenticação
+        //passando da guia application o nome e o valor
+        cy.setCookie(
+            'PrestaShop-a30a9934ef476d11b6cc3c983616e364',
+            'R6xmma6F4U6edNQuu67M0lYyG4p0n2Uj%2F%2B75zRUmzBWS%2FhpFBJtY0v0dNeXhYw4PbF5cMFyXu%2BagQjT7RandwxjVlDdhpC0yiFq9prxeX7kO25%2BLZrsKuBM7eFYMJDSjKx%2BX0hpaqVOfOTpd6B%2BM%2BnW2AfmOXAjVHSnJ8AXXBTWU70p%2FWkZQu5VE4Ylw15vkFjODpeEzNVqguWv%2BDs7EWAaRTBHlT5oK33RSpBLB5KkX8I5ObdE1tJVERFGGfEWnkdViuhrvuNPqfRS%2F8%2FRfTdC7%2BpTxPsBZXMAPVwd5k0bTeLV%2FNcPKBuMZ51lEsS0r8tspyfSWtGmpQJhUdYcn5rxpreWVe7ZSCS69H%2BCeSGVIlJ1G%2B991po9oiqTiSoyukGYD2pXk%2BAvwRTV1pn0v0ux5Zm0fP%2BUURLVMGshAigU%3D000311'
+        );
+
         //Já que o endereço foi informado no cypress.js, como URL padrão, é só colocar cy.visit('/');
         cy.visit('/');
-        
+
         //localizar pelo texto que contem na tela
         //Em seguida usar comando trigger que despare um evento o mouse ouver, para passar o mouse por cima
         let nomeProduto = 'Faded Short Sleeve T-shirts';
@@ -43,10 +51,10 @@ context('Compras', () => {
         cy.get(".cart_navigation a[href$='order&step=1']").click();
 
         //Página de login, informar as credenciais
-        cy.get('#email').type('qa@qa.com');
-        cy.get('#passwd').type('12345');
-
-        cy.get('button#SubmitLogin').click();
+  /*       cy.get('#email').type('qa@qa.com');
+        cy.get('#passwd').type('12345'); 
+        cy.get('button#SubmitLogin').click();        
+        */
 
         //validação na tela de confirmação de endereços
         //[type=checkbox]#addressesAreEquals
